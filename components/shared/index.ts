0d9ee277d4ef27e701ba628e5b53255ebe3e6d04
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-08-15 18:57:57 +0100

    feat: Refactor exports for shared folder

---
+++ components/shared/index.ts
@@ -0,0 +1,6 @@
+import { RightSidebar } from '@/components/shared';
+
+export { default as Bottombar } from '@/components/shared/Bottombar';
+export { default as Topbar } from '@/components/shared/Topbar';
+export { default as LeftSidebar } from '@/components/shared/LeftSidebar';
+export { default as RightSidebar } from '@/components/shared/RightSidebar'
