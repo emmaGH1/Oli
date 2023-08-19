46c8b77a321879c67324455caebcf8b75a66f556
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-08-19 02:10:28 +0100

    feat: Add console log message for try catch

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -11,6 +11,7 @@ export const connectToDB = async () => {
     try {
        await mongoose.connect(process.env.MONGODB_URL)
        isConnected = true
+       console.log('Connected to MongoDB')
     } catch (error) {
        console.log(error)
     }
